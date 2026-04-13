892d9d52fda17a8ded1767b069de49205e61f00d
wnsdud <150161309+nomorefifa@users.noreply.github.com>
2026-04-13 21:26:50 +0900

    [Silver II] Title: 촌수계산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+node = int(input())
+ans_x, ans_y = map(int, input().split())
+edge = int(input())
+visited = [False] * (node + 1)
+graph = [[] for _ in range(node + 1)]
+
+for i in range(edge):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+def dfs(n, ans):
+    visited[n] = True
+    for i in graph[n]:
+        if i == ans_y:
+            print(ans)
+            exit()
+        if not visited[i]:
+            dfs(i, ans + 1)
+
+dfs(ans_x, 1)
+print(-1)
