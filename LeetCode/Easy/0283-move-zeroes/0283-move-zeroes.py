fa28c2dbe1db8594f49ef09756ba661018d46310
wnsdud <150161309+nomorefifa@users.noreply.github.com>
2026-04-25 17:24:45 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.py
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.py
@@ -3,14 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        cnt_zero = 0
-        for i in nums:
-            if i == 0:
-                cnt_zero += 1
         cur_idx = 0
         for i in range(len(nums)):
             if nums[i] != 0:
                 nums[cur_idx] = nums[i]
                 cur_idx += 1
-        for i in range(cnt_zero):
-            nums[cur_idx + i] = 0
+        for i in range(cur_idx, len(nums)):
+            nums[i] = 0
