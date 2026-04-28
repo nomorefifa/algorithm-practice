aed5d1936e0daf8a8155650e99c7a995f650df25
wnsdud <150161309+nomorefifa@users.noreply.github.com>
2026-04-29 08:32:50 +0900

    [level 2] Title: 모음 사전, Time: 7.76 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,11 +1,17 @@
-from itertools import product
-
 def solution(word):
-    shit = ['A','E', 'I','O','U']
-    word_list = set()
-    for i in range(1, 6):
-        for j in product(shit, repeat = i):
-            word_list.add(''.join(j))
-    word_list = list(word_list)
-    word_list.sort()
-    return word_list.index(word) + 1
+    all_words = []
+    vowels = ['A', 'E', 'I', 'O', 'U']
+    
+    def generate(current_word):
+        if len(current_word) > 5:
+            return
+        
+        if current_word:
+            all_words.append(current_word)
+        
+        for v in vowels:
+            generate(current_word + v)
+            
+    generate("")
+    
+    return all_words.index(word) + 1
