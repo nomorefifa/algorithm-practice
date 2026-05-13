7a1ee0482cc042b70d28886e3f1446c8d845fe1a
wnsdud <150161309+nomorefifa@users.noreply.github.com>
2026-05-13 13:57:03 +0900

    Time: 0 ms (100%), Space: 21.5 MB (29.7%) - LeetHub

--- LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces.py
+++ LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces.py
@@ -3,22 +3,26 @@ sys.setrecursionlimit(2000)
 
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        visited = [False] * (len(isConnected))
-        graph = [[] for _ in range(len(isConnected))]
-        for i in range(len(isConnected)):
-            for j in range(len(isConnected)):
-                if isConnected[i][j] == 1:
-                    graph[i].append(j)
-                    graph[j].append(i)
-        def dfs(node):
-            visited[node] = True
-            for next_node in graph[node]:
-                if not visited[next_node]:
-                    dfs(next_node)
-        cnt = 0
-        for i in range(len(graph)):
-            if not visited[i]:
-                cnt += 1
-                dfs(i)
+        n = len(isConnected)
+        visited = [False] * n
         
+        def dfs(node):
+            # 1. 도시에 도착하자마자 방문 도장 쾅!
+            visited[node] = True
+            
+            # 2. 내 도시(node)와 연결된 다른 도시(i)들을 0번부터 끝까지 쭉 훑어봄
+            for i in range(n):
+                # 3. 연결되어 있고(1), 아직 안 가본 도시라면 이동!
+                if isConnected[node][i] == 1 and not visited[i]:
+                    dfs(i)
+                    
+        cnt = 0
+        
+        # 4. 0번 도시부터 마지막 도시까지 순서대로 확인
+        for i in range(n):
+            # 5. 아직 안 가본 도시를 발견했다면? 새로운 지방(무리) 발견!
+            if not visited[i]:
+                cnt += 1  # 무리 개수 +1
+                dfs(i)    # 이 도시와 연결된 모든 도시에 방문 도장 찍고 오라고 출발시킴
+                
         return cnt
