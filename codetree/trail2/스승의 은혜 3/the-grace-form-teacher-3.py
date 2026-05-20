b74debed1cba3d278307384f513256054831c862
wnsdud <150161309+nomorefifa@users.noreply.github.com>
2026-05-20 13:39:58 +0900

    [Passed] 스승의 은혜 3 (58ms, 16MB)

---
+++ codetree/trail2/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,15 @@
+N, B = map(int, input().split())
+gifts = [tuple(map(int, input().split())) for _ in range(N)]
+sum_arr = [gifts[i][0] + gifts[i][1] for i in range(N)]
+ans = 0
+for i in range(N):
+    tmpCnt = 1
+    tmpB = B - (gifts[i][0]) // 2 - gifts[i][1]
+    tmpGifts = sorted(sum_arr[0:i] + sum_arr[i + 1:], key = lambda x: x)
+    for j in range(len(tmpGifts)):
+        if tmpB - tmpGifts[j] >= 0:
+            tmpB -= tmpGifts[j]
+            tmpCnt += 1
+    if tmpCnt > ans:
+        ans = tmpCnt
+print(ans)
