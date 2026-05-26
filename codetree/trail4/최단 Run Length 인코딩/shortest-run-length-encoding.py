8c11935a1ce938ce422c624c4f64ad49908ed05c
wnsdud <150161309+nomorefifa@users.noreply.github.com>
2026-05-26 22:57:44 +0900

    [Passed] 최단 Run Length 인코딩 (47ms, 16MB)

--- codetree/trail4/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ codetree/trail4/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -5,18 +5,20 @@ ans = float('1e9')
 for i in range(len(a)):
     cur_word = a[-1] + a[:len(a) - 1]
     a = cur_word
-
+    ans_word = ""
     pre = a[0]
+    ans_word += pre
     cnt = 1
     for j in range(1, len(a)):
         if a[j] != pre:
+            ans_word += str(cnt)
+            ans_word += a[j]
+            cnt = 1
+        else:
             cnt += 1
         pre = a[j]
-    
-    if cnt < ans:
-        ans = cnt
+    ans_word += str(cnt)
+    if len(ans_word) < ans:
+        ans = len(ans_word)
 
-if ans == 1 and len(a) == 10:
-    print(3)
-else:
-    print(ans * 2)
+print(ans)
