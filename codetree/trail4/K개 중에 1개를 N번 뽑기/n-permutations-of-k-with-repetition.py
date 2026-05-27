382a594245fcecaf4501598762990f918f3e0acb
wnsdud <150161309+nomorefifa@users.noreply.github.com>
2026-05-27 10:06:05 +0900

    [Passed] K개 중에 1개를 N번 뽑기 (129ms, 23MB)

---
+++ codetree/trail4/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.py
@@ -0,0 +1,14 @@
+k, n = map(int, input().split())
+ans = []
+
+def choose(cnt):
+    if cnt == n:
+        print(" ".join(ans))
+        return
+
+    for i in range(1, k + 1):
+        ans.append(str(i)) 
+        choose(cnt + 1)
+        ans.pop()
+
+choose(0)
