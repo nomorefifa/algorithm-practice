8cad26af10eaff5d3bc5a8f307ec88aa36dd7a14
wnsdud <150161309+nomorefifa@users.noreply.github.com>
2026-08-09 12:29:48 +0900

    [level 2] Title: 전화번호 목록, Time: 599.38 ms, Memory: 30.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,14 @@
 def solution(phone_book):
+    phone_dict = {}
     ans = True
-    phone_book.sort()
-    for i in range(len(phone_book) - 1):
-        if phone_book[i] == phone_book[i + 1][:len(phone_book[i])]:
-            ans = False
-            break
+    for num in range(len(phone_book)):
+        phone_dict[phone_book[num]] = 1
+    for i in range(len(phone_book)):
+        tmp_num = ""
+        phone_dict[phone_book[i]] = 0
+        for j in range(len(phone_book[i])):
+            tmp_num += phone_book[i][j]
+            if tmp_num in phone_dict and phone_dict[tmp_num] == 1:
+                ans = False
+        phone_dict[phone_book[i]] = 1
     return ans
