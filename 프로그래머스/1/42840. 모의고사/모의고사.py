440cb705d424adb7838ad41fe452f4b5837eb18f
wnsdud <150161309+nomorefifa@users.noreply.github.com>
2026-08-18 23:40:05 +0900

    [level 1] Title: 모의고사, Time: 1.53 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,18 +1,21 @@
 def solution(answers):
-    a = [1, 2, 3, 4, 5]
-    b = [2, 1, 2, 3, 2, 4, 2, 5]
-    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    ans = [0, 0, 0]
+    p1 = [1, 2, 3, 4, 5]
+    p2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    cnt1, cnt2, cnt3 = 0, 0, 0
     for i in range(len(answers)):
-        if answers[i] == a[i % 5]:
-            ans[0] += 1
-        if answers[i] == b[i % 8]:
-            ans[1] += 1
-        if answers[i] == c[i % 10]:
-            ans[2] += 1
-    tmp = max(ans)
-    t = []
-    for i in range(3):
-        if ans[i] == tmp:
-            t.append(i + 1)
-    return t
+        if answers[i] == p1[i % len(p1)]:
+            cnt1 += 1
+        if answers[i] == p2[i % len(p2)]:
+            cnt2 += 1
+        if answers[i] == p3[i % len(p3)]:
+            cnt3 += 1
+    ans_num = max(cnt1, cnt2, cnt3)
+    ans = []
+    if cnt1 == ans_num:
+        ans.append(1)
+    if cnt2 == ans_num:
+        ans.append(2)
+    if cnt3 == ans_num:
+        ans.append(3)
+    return ans
