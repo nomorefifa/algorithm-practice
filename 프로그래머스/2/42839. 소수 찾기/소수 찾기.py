053739746d72f7ae589cb5f711b20aa090c49e4e
wnsdud <150161309+nomorefifa@users.noreply.github.com>
2026-08-19 21:45:45 +0900

    [level 2] Title: 소수 찾기, Time: 1162.93 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,24 +1,25 @@
 from itertools import permutations
 
+def isprime(num):
+    if num < 2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
 def solution(numbers):
-    numlist = list(numbers)
-    set_num = set()
-    cnt = 0
-    for i in range(1, len(numlist) + 1):
-        tmp = list(permutations(numlist, i))
-        for j in range(len(tmp)):
-            cur_num = ''
-            for k in range(i):
-                cur_num += tmp[j][k]
-            cur_num = int(cur_num)
-            set_num.add(cur_num)
-    for num in set_num:
-        if num < 2: continue
-        flag = True
-        for prime in range(2, num):
-            if num % prime == 0:
-                flag = False
-                break
-        if flag:
-            cnt += 1
-    return cnt
+    visited = [False] * len(numbers)
+    prime_set = set()
+    def dfs(num):
+        for i in range(len(numbers)):
+            if visited[i] == True:
+                continue
+            visited[i] = True
+            cur_num = num * 10 + int(numbers[i])
+            if isprime(cur_num):
+                prime_set.add(cur_num)
+            dfs(cur_num)
+            visited[i] = False
+    dfs(0)
+    return len(prime_set)
