a49a206b6ad41ae916555c9174613ac1d86d25f0
wnsdud <150161309+nomorefifa@users.noreply.github.com>
2026-08-20 19:17:40 +0900

    [level 2] Title: 피로도, Time: 17.12 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,27 +1,11 @@
 def solution(k, dungeons):
     visited = [False] * len(dungeons)
-    answer = 0
-
     def dfs(k, cnt):
-        nonlocal answer
-
-        answer = max(answer, cnt)
-
+        ans = cnt
         for i in range(len(dungeons)):
-            if visited[i]:
-                continue
-
-            minimum, consume = dungeons[i]
-
-            if k < minimum:
-                continue
-
-            visited[i] = True
-
-            dfs(k - consume, cnt + 1)
-
-            visited[i] = False
-
-    dfs(k, 0)
-
-    return answer
+            if not visited[i] and k >= dungeons[i][0]:
+                visited[i] = True
+                ans = max(ans, dfs(k - dungeons[i][1], cnt + 1))
+                visited[i] = False
+        return ans
+    return dfs(k, 0)
